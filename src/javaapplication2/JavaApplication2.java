3f24d3b1beba707390f6e377f5043a15ede388ae
bhaijaan <rajpurena@gmail.com>
2019-10-17 21:23:16 +0530

    1st commit.

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author b
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static String school = "global school";
+    public static void main(String[] args) {
+        String name = "ram";
+        int age = 21;
+        int grade = 3;
+        System.out.println("name : "+name+"\n age : "+age+"\n grade : "+grade +"\nschool : "+school);
+    }
+    
+}
